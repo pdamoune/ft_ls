291cb51fd972c028122b92d17ae486f054aaae7d
Philippe DAMOUNE <pdamoune@e2r7p17.42.fr>
2018-03-08 18:33:43 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 10:40:26 by pdamoune          #+#    #+#             */
-/*   Updated: 2018/02/06 18:59:04 by pdamoune         ###   ########.fr       */
+/*   Updated: 2018/03/08 17:09:39 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,13 @@ int		main(int ac, char **av)
 {
 
 	t_data_template	data;
+	int fd;
 
 	ft_bzero(&data, sizeof(t_data_template));
 	if (cliopts_get(av, g_read_opts, &data))
 		return (1);
+	fd = open("/dev/ttys002", O_RDWR | O_NOCTTY | O_SYNC);
+	ft_putnbrel(ft_dprintf(fd, "coucou"));
 	return (0);
 	(void)ac;
 }
